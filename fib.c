7cd5e8dcc0fe29850e2a5d4d963164dee07e225e
floating-rice-farmer <magicalelectropants@gmail.com>
2026-03-06 16:14:52 +1100

    e
    d

--- fib.c
+++ fib.c
@@ -1,38 +1,70 @@
 #include <stdio.h>
 #include <gmp.h>
+#include <omp.h>
 #include <time.h>
 
-// For calculating unreasonably high fibonacci numbers for whatever reason
+void parallel_fib(unsigned long n, mpz_t a, mpz_t b) {
+    if (n == 0) {
+        mpz_set_ui(a, 0);
+        mpz_set_ui(b, 1);
+        return;
+    }
+
+    mpz_t f_k, f_k1, t1, t2;
+    mpz_inits(f_k, f_k1, t1, t2, NULL);
+
+    parallel_fib(n >> 1, f_k, f_k1);
+
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        {
+            mpz_mul_2exp(t1, f_k1, 1);
+            mpz_sub(t1, t1, f_k);
+            mpz_mul(t1, t1, f_k); 
+        }
+        #pragma omp section
+        {
+            mpz_t f_k_sq, f_k1_sq;
+            mpz_inits(f_k_sq, f_k1_sq, NULL);
+            mpz_mul(f_k_sq, f_k, f_k);
+            mpz_mul(f_k1_sq, f_k1, f_k1);
+            mpz_add(t2, f_k1_sq, f_k_sq);
+            mpz_clears(f_k_sq, f_k1_sq, NULL);
+        }
+    }
+
+    if (n & 1) {
+        mpz_set(a, t2);
+        mpz_add(b, t1, t2);
+    } else {
+        mpz_set(a, t1);
+        mpz_set(b, t2);
+    }
+
+    mpz_clears(f_k, f_k1, t1, t2, NULL);
+}
 
 int main() {
     unsigned long n;
-    printf("Enter Fibonacci index: ");
     if (scanf("%lu", &n) != 1) return 1;
 
-    mpz_t result;
-    mpz_init(result);
+    mpz_t result, next;
+    mpz_inits(result, next, NULL);
 
-    clock_t start = clock();
-    mpz_fib_ui(result, n);
-    clock_t end = clock();
+    double start = omp_get_wtime();
+    parallel_fib(n, result, next);
+    double end = omp_get_wtime();
 
     FILE *f = fopen("fibo.txt", "w");
-    if (f == NULL) {
-        fprintf(stderr, "Error: Could not open fibo.txt for writing.\n");
-        mpz_clear(result);
-        return 1;
+    if (f) {
+        gmp_fprintf(f, "%Zd\n", result);
+        fclose(f);
     }
 
-    gmp_fprintf(f, "%Zd\n", result);
-    fclose(f);
+    printf("Time: %f s\n", end - start);
+    printf("Digits: %zu\n", mpz_sizeinbase(result, 10));
 
-    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    size_t digits = mpz_sizeinbase(result, 10);
-
-    printf("\nSuccess! Result written to fibo.txt\n");
-    printf("Digits: %zu\n", digits);
-    printf("Calculation time: %f seconds\n", time_spent);
-
-    mpz_clear(result);
+    mpz_clears(result, next, NULL);
     return 0;
 }
